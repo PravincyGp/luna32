da4cd016d1da26f28ad27ab593e78de2be87f88c
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-02-04 22:58:21 +0530

    Update nightmode.py

--- EmikoRobot/modules/nightmode.py
+++ EmikoRobot/modules/nightmode.py
@@ -154,7 +154,7 @@ async def job_open():
         try:
             await tbot.send_message(
                 int(pro.chat_id),
-                "06:00 Am, Group Is Opening.\n**Powered By @lunasupportz**",
+                "06:00 Am, Group Is Opening.\n**Powered By @Rosisupport**",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
